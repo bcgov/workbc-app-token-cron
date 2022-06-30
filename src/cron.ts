bfaeccad93e7d797d2c630c3a8ff5b4a445d0b38
Branko Bajic <branko@mailfence.com>
2022-06-29 23:09:29 -0700

    Initial DB read

--- src/cron.ts
+++ src/cron.ts
@@ -1,7 +1,11 @@
+import { QueryResult } from "pg"
+
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const helmet = require("helmet")
 const cors = require("cors")
+const cron = require("node-cron")
+const db = require("./config/db.config")
 
 const originURL = process.env.ORIGIN_URL || process.env.OPENSHIFT_NODEJS_ORIGIN_URL || "https://localhost:8000"
 
@@ -17,10 +21,25 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(helmet())
+console.log("===== CRON SERVICE STARTED =====")
 
-const port = process.env.PORT || "8000"
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`)
+cron.schedule("10 * * * * *", async () => {
+    console.log("===== START TOKEN CLEANUP =====")
+
+    try {
+        await db.query(
+            `
+            SELECT t.token_id, t.user_id, t.token, t.platform
+            FROM  tokens t
+            `,
+            []
+        )
+            .then(async (tokens: QueryResult) => {
+                console.log(tokens.rows)
+            })
+    } catch (e: any) {
+        console.log(e.message)
+    }
+
+    console.log("===== END TOKEN CLEANUP =====")
 })
-
-module.exports = app
